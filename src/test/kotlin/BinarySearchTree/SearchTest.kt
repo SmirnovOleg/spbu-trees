35f775fef55e4adc8a7d5cfc7ece55743bb19000
Kuzivanov Sergey <43182277+SergeyKuz1001@users.noreply.github.com>
2019-04-28 09:36:00 +0300

    random tree create
    Сделал тестирование на рандомном дереве

--- src/test/kotlin/BinarySearchTree/SearchTest.kt
+++ src/test/kotlin/BinarySearchTree/SearchTest.kt
@@ -6,36 +6,39 @@ import java.util.Random
 
 class BinarySearchTreeSearchTest {
 
-    private val values: MutableList<Int> = mutableListOf()
-    private val keys = 1..1000
-    private val nonExistentKeys = 1001..2000
+    private val expectedAnswers = mutableMapOf<Int, Int>()
     private val actualTree = BinarySearchTree<Int, Int>()
     private val random = Random()
 
     @Before
     fun createTreeForSearching() {
-        for (key in keys) {
-            values.add(random.nextInt())
-            actualTree[key] = values.last()
+        while (expectedAnswers.size < 10000) {
+            val key = random.nextInt()
+            val value = random.nextInt()
+            expectedAnswers[key] = value
+            actualTree[key] = value
         }
     }
 
     @Test
     fun searchForAll() {
-        for (key in keys)
-            assertEquals(actualTree[key], values[key - 1])
+        for (key in expectedAnswers.keys)
+            assertEquals(actualTree[key], expectedAnswers[key])
     }
 
     @Test
     fun searchForNonExistentKey() {
-        for (key in nonExistentKeys)
-            assertNull(actualTree[key])
+        for (key in 1..20000) {
+            if ( ! (key in expectedAnswers.keys))
+                assertNull(actualTree[key])
+        }
     }
 
     @Test
     fun searchInEmptyTree() {
         val emptyTree = BinarySearchTree<Int, Int>()
-        assertNull(emptyTree[keys.first])
+        for (key in 1..10000)
+            assertNull(actualTree[key])
     }
 
 }
